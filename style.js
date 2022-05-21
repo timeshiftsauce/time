df384acc3c07a12ce7966934c379812bd72e37e0
timeshiftsauce <104637375+timeshiftsauce@users.noreply.github.com>
2022-05-21 16:02:03 +0800

    Add files via upload

---
+++ style.js
@@ -0,0 +1,15 @@
+{
+  "name": "时迁酱的home",
+  "short_name": "时迁酱",
+  "display": "standalone",
+  "start_url": "/",
+  "theme_color": "#8888ff",
+  "background_color": "#aaaaff",
+  "icons": [
+    {
+      "src": "./zhouchenlu/mysqj-Home/image/head.png",
+      "sizes": "256x256",
+      "type": "./zhouchenlu/mysqj-Home/image/head.png"
+    }
+  ]
+}
